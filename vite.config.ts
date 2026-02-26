8efd0027111fda8bf253a1e9d96f4b6aafa4e3af
Saarthak Patil <saarthakpatil314@gmail.com>
2026-02-27 03:44:53 +0530

    Allow all hosts in Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    allowedHosts: 'all'
+  },
+
   build: {
     chunkSizeWarningLimit: 1000,
   },
 })
-
